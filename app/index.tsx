ec2c380ad7d333e35ecc6743cb01204bc36fcb48
Geddam Shyam Gowtham <geddamgowtham04@outlook.com>
2025-04-04 11:50:08 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="flex items-center justify-center text-2xl text-blue-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
